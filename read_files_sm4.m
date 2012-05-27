b04d1491345c66a8fb755a578c1df8d97ea5e6c0
MartinC <caldarolamartin@gmail.com>
2012-05-27 17:53:33 -0300

    String data for the first page: DONE

--- read_files_sm4.m
+++ read_files_sm4.m
@@ -5,18 +5,18 @@ data=read_sm4(filename);
 
 
 %%
-file_header=data{1}; % Total page count and object list count
-object_list=data{2};
-page_index_header=data{3};
-page_index=data{4};
-page_index_array=data{5};
-page_header=data{6};
-object_list_string=data{7};
-
+file_header = data{1}; % Total page count and object list count
+object_list = data{2};
+page_index_header = data{3};
+page_index = data{4};
+page_index_array = data{5};
+page_header = data{6};
+object_list_string = data{7};
+string_data = data{8};
 
 %%
 
-object_list_string(end)
+string_data
 
 
 
